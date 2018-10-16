0f13c0dd828f46491b963d83dc6c9ff4a2676003
Kenny Rodríguez <effexts@gmail.com>
2018-10-16 10:00:26 -0300

    some data

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 /** @format */
+import React from 'react';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'ClubDeportivo'} />
+    <AlbumList />
+  </View>
+);
 
-AppRegistry.registerComponent(appName, () => App);
+//import { AppRegistry } from 'react-native';
+//import App from './App';
+//import { name as appName } from './app.json';
+
+//AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('ClubDeportivo', () => App);
